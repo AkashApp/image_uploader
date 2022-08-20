0c8cafc3bc813fb807cb5b88954793f8147867c9
Akash Borate <27akashborate@gmail.com>
2022-08-20 13:36:22 +0530

    completed the project

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,16 @@ function App() {
     "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"
   );
 
+  const imageHandler = (e) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setProfileImage(reader.result);
+      }
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  };
+
   return (
     <div className="page">
       <div className="container">
@@ -14,7 +24,13 @@ function App() {
         <div className="img-holder">
           <img src={profileImage} alt="" id="img" className="img" />
         </div>
-        <input type="file" name="image-upload" id="input" accept="image/*" />
+        <input
+          type="file"
+          name="image-upload"
+          id="input"
+          accept="image/*"
+          onChange={imageHandler}
+        />
         <div className="label">
           <label htmlFor="input" className="image-upload">
             <h3>&#128247;</h3>
